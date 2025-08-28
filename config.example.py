1cafcf6c7c1b28ee7e2f5441fcf3ec9492e633d2
A3incSTIGMAT <brilsash@yandex.ru>
2025-08-28 11:06:10 +0800

    Create config.example.py

---
+++ config.example.py
@@ -0,0 +1,8 @@
+# config.example.py — Шаблон настроек
+BOT_TOKEN = "123456789:ABCDEFGHIJKLMNOPQRSTUVWXYZ"  # Замени на свой токен
+CURRENCY = "RUB"
+
+# Free-Kassa
+MERCHANT_ID = 123456  # Замени на свой
+SECRET_1 = "your_secret1"  # Секретное слово 1
+SECRET_2 = "your_secret2"  # Секретное слово 2
