74ae6b340f5fc84651f432f889944d08f5ece6e4
A3incSTIGMAT <brilsash@yandex.ru>
2026-03-23 11:29:46 +0800

    Delete config.example.py

--- config.example.py
+++
@@ -1,9 +0,0 @@
-# config.example.py — Шаблон настроек
-BOT_TOKEN = "ваш_токен_от_botfather"
-CURRENCY = "RUB"
-
-# Free-Kassa
-MERCHANT_ID = 64786
-SECRET_1 = "YOHO&]cA-i6]Q1-"
-SECRET_2 = "Ej+7]0)%8u1N$jj"
-
