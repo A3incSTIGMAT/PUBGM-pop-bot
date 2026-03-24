ea021495d6d4e09daf629aec64988742297f880b
A3incSTIGMAT <brilsash@yandex.ru>
2026-03-24 18:46:49 +0800

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,8 +2,9 @@ from . import admin
 from . import user
 from . import games
 from . import economy
-from . import balance_handler  # ← НОВЫЙ ИМПОРТ
+from . import balance_handler
 from . import report
 from . import instructions
 from . import callbacks
 from . import roles
+from . import birthday_handler  # ← ДОБАВИТЬ
