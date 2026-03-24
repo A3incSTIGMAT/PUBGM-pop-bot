f3aebf20d0f68b0ba44259eae58f041351cadeda
A3incSTIGMAT <brilsash@yandex.ru>
2026-03-24 20:18:45 +0800

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,5 @@
 from . import admin
 from . import user
-# from . import games  # ← УДАЛИТЬ или закомментировать
 from . import economy
 from . import balance_handler
 from . import report
@@ -9,4 +8,4 @@ from . import callbacks
 from . import roles
 from . import birthday_calendar
 from . import games_interactive
-from . import menu_handler
+# menu_handler НЕ импортируем здесь!
