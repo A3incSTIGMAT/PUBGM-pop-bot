8264cdacd3607a24c43879690b37574a15b918fc
A3incSTIGMAT <brilsash@yandex.ru>
2026-03-24 18:50:16 +0800

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -7,4 +7,5 @@ from . import report
 from . import instructions
 from . import callbacks
 from . import roles
-from . import birthday_handler  # ← ДОБАВИТЬ
+from . import birthday_handler
+from . import games_interactive  # ← ДОБАВИТЬ
