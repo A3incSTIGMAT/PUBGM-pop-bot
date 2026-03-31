34738c660c563e2136440dca1350f1bf32127fcd
A3incSTIGMAT <brilsash@yandex.ru>
2026-03-31 11:05:27 +0800

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,10 @@
+"""
+Utils package for Nexus Bot
+"""
+
+from . import logger
+from . import keyboards
+from . import helpers
+from . import smart_parser
+
+__all__ = ['logger', 'keyboards', 'helpers', 'smart_parser']
