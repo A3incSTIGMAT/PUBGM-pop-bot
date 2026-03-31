873673fba0b39e9f973bcc66f722687587f8f8d9
A3incSTIGMAT <brilsash@yandex.ru>
2026-03-31 11:10:58 +0800

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,14 +1,23 @@
-from . import admin
-from . import user
+"""
+Handlers package for Nexus Bot
+"""
+
+from . import start
+from . import profile
 from . import economy
-from . import games_interactive
-from . import birthday_calendar
-from . import menu_handler
-from . import shop
-from . import rp_commands
+from . import games
+from . import moderation
+from . import stats
+from . import social
 from . import vip
-from . import report
-from . import instructions
-from . import callbacks
-from . import roles
-from . import smart_parser
+from . import ai_assistant
+from . import payments
+from . import shop
+from . import help
+from . import about
+from . import smart_commands
+
+__all__ = [
+    'start', 'profile', 'economy', 'games', 'moderation', 'stats',
+    'social', 'vip', 'ai_assistant', 'payments', 'shop', 'help', 'about', 'smart_commands'
+]
